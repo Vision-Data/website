6034ac2da37136349f1d648c36403b83c4f2107d
Jerem Dlcn <44464165+JeremDlcn@users.noreply.github.com>
2022-11-18 15:27:57 +0100

    insert informations avout articles

--- article/article.js
+++ article/article.js
@@ -3,41 +3,28 @@ import '../styles/style.styl'
 
 
 
-async function getArticles() {
-    let articles = [];
-    const response = await fetch(`https://blog.vision-data.io/items/articles_vision?fields=id,title,thumbnail,introduction`, {
+async function getData() {
+    let article = [];
+    
+    //get query params id
+    const urlParams = new URLSearchParams(window.location.search);
+    const id = urlParams.get('id');
+
+    const response = await fetch(`https://blog.vision-data.io/items/articles_vision/${id}?fields=title,thumbnail,introduction,content,date_created,date_updated,tags_vision.tags_id.name,sources_vision.sources_id.*`, {
         method: 'GET'
     });
     const data = await response.json();
-    articles = data.data;
-    return articles;
+    article = data.data;
+    return article;
 }
 
 
-function createArticle(article) {
-    const articleElement = document.createElement('li');
-    articleElement.classList.add('news-item');
-    articleElement.innerHTML = `
-    <a href="${article.id}" class="news-link">
-        <div class="news-article">
-            <div class="news-image"><img src="https://blog.vision-data.io/assets/${article.thumbnail}" alt=""/></div>
-            <div class="news-infos">
-                <h2 class="news-title">${article.title}</h2>
-                <p class="news-hook">${article.introduction}</p>
-            </div>
-        </div>
-    </a>
-    `;
-    return articleElement;
-}
-
-
-console.log(await getArticles());
-
-const articles = await getArticles();
-const articlesContainer = document.querySelector('#news-container');
-articles.forEach(article => {
-    const articleElement = createArticle(article);
-    console.log(articleElement);
-    articlesContainer.appendChild(articleElement);
-});
+const article = await getData();
+const articleDate = new Date(article.date_updated).toLocaleDateString('fr-FR', {day: 'numeric', month: 'long', year: 'numeric'});
+const articleTitle = document.querySelector('.article-title').innerHTML = article.title;
+const articleThumbnail = document.querySelector('.article-thumbnail-img').src = "https://blog.vision-data.io/assets/" + article.thumbnail;
+const articleIntroduction = document.querySelector('.article-accroche').innerHTML = article.introduction;
+const articleContent = document.querySelector('.article-content').innerHTML = article.content;
+const articleDateUpdated = document.querySelector('.article-date').innerHTML = articleDate;
+const articleTags = document.querySelector('.article-tags').innerHTML = article.tags_vision.tags_id.name;
+const articleSources = document.querySelector('.article-sources').innerHTML = article.sources_vision.sources_id.name;
