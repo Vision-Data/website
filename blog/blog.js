67150bcc700b44f30a8709c5c505658de884a1d9
Jerem Dlcn <44464165+JeremDlcn@users.noreply.github.com>
2022-11-18 19:05:23 +0100

    query possibilites

--- blog/blog.js
+++ blog/blog.js
@@ -1,28 +1,28 @@
 import '../styles/tailwind.css'
 import '../styles/style.styl'
 
-
+init();
+window.addEventListener('hashchange', () => {
+    init();
+});
 
 async function getArticles() {
     let articles = [];
     const urlParams = new URLSearchParams(window.location.search);
     const queryTag = urlParams.get('tag');
     if (queryTag) {
-        console.log('ici');
         const response = await fetch(`https://blog.vision-data.io/items/articles_vision?fields=id,title,thumbnail,introduction,tags.tags_id.*&filter[tags][tags_id][id][_eq]=${queryTag}`, {
             method: 'GET'
         });
         const data = await response.json();
         articles = data.data;
-        console.log(articles);
     } else {
-        console.log('normal');
         const response = await fetch(`https://blog.vision-data.io/items/articles_vision?fields=id,title,thumbnail,introduction`, {
             method: 'GET'
         });
         const data = await response.json();
         articles = data.data;
-        
+
     }
     return articles;
 }
@@ -45,10 +45,30 @@ function createArticle(article) {
     return articleElement;
 }
 
-
-const articles = await getArticles();
-const articlesContainer = document.querySelector('#news-container');
-articles.forEach(article => {
-    const articleElement = createArticle(article);
-    articlesContainer.appendChild(articleElement);
-});
+async function init() {
+    const articles = await getArticles();
+    const articlesContainer = document.querySelector('#news-container');
+    
+    if (articles.length === 0) {
+        articlesContainer.innerHTML = `<p class="news-empty">Aucun article ne correspond à votre recherche</p><div class="article-all">
+        <button class="btn btn-second">
+            <a href="/blog/index.html" class="article-see-all">Voir tout les articles</a>
+        </button>
+    </div>`;
+    } else {
+        const urlParams = new URLSearchParams(window.location.search);
+        const queryTag = urlParams.get('tag');
+        articles.forEach(article => {
+            const articleElement = createArticle(article);
+            articlesContainer.appendChild(articleElement);
+        });
+        if (queryTag) {
+            articlesContainer.innerHTML += `
+            <div class="article-all">
+            <button class="btn btn-second">
+                <a href="/blog/index.html" class="article-see-all">Voir tout les articles</a>
+            </button>
+        </div>`;
+        }
+    }
+}
