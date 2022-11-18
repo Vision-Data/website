36d48572abadf8ea2e223e3f0671c1878c776563
Jerem Dlcn <44464165+JeremDlcn@users.noreply.github.com>
2022-11-18 14:06:11 +0100

    add structure for articles

--- blog/blog.js
+++ blog/blog.js
@@ -1,15 +1,43 @@
 import '../styles/tailwind.css'
 import '../styles/style.styl'
 
-console.log(getArticles());
 
-function getArticles() {
+
+async function getArticles() {
     let articles = [];
-    fetch(`https://blog.vision-data.io/items/articles_vision`, {
-         method: 'GET'
-    }).then(response => response.json())
-    .then(data => {
-        articles = data.data;
-    })
+    const response = await fetch(`https://blog.vision-data.io/items/articles_vision?fields=id,title,thumbnail,introduction`, {
+        method: 'GET'
+    });
+    const data = await response.json();
+    articles = data.data;
     return articles;
 }
+
+
+function createArticle(article) {
+    const articleElement = document.createElement('li');
+    articleElement.classList.add('news-item');
+    articleElement.innerHTML = `
+    <a href="${article.id}" class="news-link">
+        <div class="news-article">
+            <div class="news-image"><img src="https://blog.vision-data.io/assets/${article.thumbnail}" alt=""/></div>
+            <div class="news-infos">
+                <h2 class="news-title">${article.title}</h2>
+                <p class="news-hook">${article.introduction}</p>
+            </div>
+        </div>
+    </a>
+    `;
+    return articleElement;
+}
+
+
+console.log(await getArticles());
+
+const articles = await getArticles();
+const articlesContainer = document.querySelector('#news-container');
+articles.forEach(article => {
+    const articleElement = createArticle(article);
+    console.log(articleElement);
+    articlesContainer.appendChild(articleElement);
+});
